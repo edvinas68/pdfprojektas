0158534f9b691bb14f96acd4fcf036be816b71e7
edvinas68 <edvinas.juodeika@stud.smk.lt>
2022-02-23 14:43:47 +0200

    manteliopushas

--- resources/views/seller.blade.php
+++ resources/views/seller.blade.php
@@ -6,3 +6,176 @@
     </div>
 </x-app-layout>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="pasirinkimai.css">
+    <title>Pasirinkimu langas</title>
+</head>
+<body>
+    
+    <div class="purchase text-center">
+        <h1>Automatizuota dokumentų kūrimo sistema</h1>
+        <p>
+          Pasirinkite kokį dokumentą noritę sukurti.
+        </p>
+
+
+        <div class="topnav">
+  <a class="active" href="#home">Pagrindinis</a>
+  
+  </div> 
+
+
+        <div style="height: 100px;">
+        
+        <div class="cards">
+          <div class="d-flex flex-row justify-content-center flex-wrap">
+            
+            </div>
+         </div>
+         <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Įdarbinimo forma</h5>
+                </div>
+                <p class="card-text">
+                  Prašymo/Įsakymo priemimo formos pildymas.
+                </p>
+                <div class="pricing">
+                <br>
+                
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+
+            <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Atleidimo forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo Atleidimo formos pildymas.
+                </p>
+                <div class="pricing">
+                <br>
+
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Apmokamos atostogų forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo apmokamų atostogų formos pildymas.
+                </p>
+                <div class="pricing">
+                  <br>
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Nemokamų atostogų forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo nemokamų atostogų formos pildymas.
+                </p>
+                <div class="pricing">
+                <br>
+
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Vaiko priežiūros atostogų forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo vaiko priežiūros atostogų formos pildymas.
+                </p>
+                <div class="pricing">
+                  <br>
+
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Pravaikštų forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo Pravaikštų formos pildymas.
+                </p>
+                <div class="pricing">
+                  <br>
+
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+                <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Išvykimo į komandiruotę forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo išvykimo į komandiruotę formos pildymas.
+                </p>
+                <div class="pricing">
+                  <br>
+
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+                <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Priedų forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo priedų formos pildymas.
+                </p>
+                <div class="pricing">
+                  <br>
+                  
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+                <div class="card">
+              <div class="card-body">
+                <div class="title">
+                  <h5 class="card-title">Darbo sąlygų keitimo forma</h5>
+                </div>
+                <p class="card-text">
+                Prašymo/Įsakymo darbo sąlygų keitimo formos pildymas.
+                </p>
+                <div class="pricing">
+                <br>
+                
+                  <a href="index.blade.php" class="btn btn-dark px-5 py-2 primary-btn mb-5">Pildyti</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+</body>
+</html>
